fa6bc52eb85407e5c1ea6ced74042a24b3838511
Rob Buels <rmb32@cornell.edu>
2010-02-18 08:14:16 +0000

    parallel gth to xml converter mostly debugged
    git-svn-id: svn+ssh://svn.sgn.cornell.edu/cxgn/ITAG/trunk@17245 b4ddb4e3-33f1-0310-aa89-e703427ae9b8

--- hacks/itag_remap_xml2gff3.pl
+++ hacks/itag_remap_xml2gff3.pl
@@ -4,7 +4,6 @@ use warnings;
 use autodie ':all';
 
 use Carp;
-
 use Getopt::Std;
 
 use 5.10.0;
@@ -30,6 +29,11 @@ package parallel_gthxml_to_gff3;
 use Moose;
 use MooseX::Types::Path::Class;
 use File::Flock;
+use Path::Class;
+use autodie ':all';
+
+use POSIX;
+use DB_File;
 
 with 'MooseX::Workers';
 
@@ -61,9 +65,11 @@ has 'files_done' => ( is => 'ro',
 
                          return {} unless -f $self->donefile;
 
-                         my %done;
+			 print "indexing donefile ... ";
+                         tie my %done, 'DB_File', $self->donefile.'.index', O_CREAT|O_RDWR;
                          my $d = $self->donefile->openr;
                          $done{ $_ } = 1 while <$d>;
+			 print "done.\n";
                          return \%done;
                      }
 
@@ -78,53 +84,19 @@ has 'find_handle' => ( is => 'ro',
                        isa => 'FileHandle',
                        lazy_build => 1,
                       ); sub _build_find_handle {
-                          open my $f, "find ".join(' ',@{shift->dirs})." -type f -and -name '*.xml' |";
+                          open my $f, "ssh eggplant find ".join(' ',map dir($_)->absolute, @{shift->dirs})." -type f -and -name '*.xml' |";
                           return $f;
                       }
-
-sub _queue_jobs {
-    my $self = shift;
-
-    my $files = $self->find_handle;
-
-    while( my $xml_file = <$files> ) {
-	chomp $xml_file;
-	if( $self->is_done($xml_file) ) {
-	    warn "file $xml_file already done, skipping.\n";
-	    next;
-	}
-
-	$self->enqueue(sub {
-            my $gff3_out_file = File::Temp->new;
-	    $gff3_out_file->close;
-	    eval {
-                GTH_XML_2_GFF3->gthxml_to_gff3( $xml_file, "$gff3_out_file" );
-	    };
-	    if( $@ ) {
-                warn "$xml_file parse failed:\n$@";
-	    } else {
-                # dump the converted results to our output file
-                open my $gff3_fh,'<', "$gff3_out_file";
-                { my $l = File::Flock->new( $self->outfile );
-                  my $out_fh = $self->outfile->open('>>');
-                  $out_fh->print($_) while <$gff3_fh>;
-                }
-
-                # record this file as done
-                { my $l = File::Flock->new( $self->donefile );
-                  $self->donefile->openw->print("$xml_file\n")
-                }
-	    }
-	});
-    }
-}
 
 # wrap MX::Workers enqueue to just take an xml file name, and generate
 # the worker for it
 around enqueue => sub {
     my ( $orig, $self, $xml_file ) = @_;
 
-    return if $self->is_done( $xml_file );
+    if( $self->is_done( $xml_file ) ) {
+	print "$xml_file skipped\n";
+    }
+    print "$xml_file queued ...\n";
 
     $self->$orig(sub {
         my $gff3_out_file = File::Temp->new;
@@ -139,22 +111,34 @@ around enqueue => sub {
             open my $gff3_fh,'<', "$gff3_out_file";
             { my $l = File::Flock->new( $self->outfile );
               my $out_fh = $self->outfile->open('>>');
-              $out_fh->print($_) while <$gff3_fh>;
+	      while( <$gff3_fh> ) {
+		  unless( /^##gff-version/ ) {
+		      $out_fh->print($_);
+		  }
+	      }
           }
 
             # record this file as done
             { my $l = File::Flock->new( $self->donefile );
-              $self->donefile->openw->print("$xml_file\n")
-          }
+              $self->donefile->open('>>')->print("$xml_file\n")
+	    }
         }
     });
 };
 
 sub run {
     my $self = shift;
+
+    # run find first
     my $files = $self->find_handle;
+
+    # print just one gff3 header in the outfile
+    $self->outfile->open('>>')->print("##gff-version 3\n");
+
+    # now queue the first set of jobs
     for (1 .. $self->max_workers ) {
         my $xml_file = <$files>;
+	chomp $xml_file;
         last unless defined $xml_file;
         $self->enqueue($xml_file);
     }
@@ -164,11 +148,11 @@ sub run {
 #sub worker_done    { shift; warn join ' ', @_; }
 sub worker_done {
     my $self = shift;
-    my $files = $self->find_handle;
-    while (1) {
-        my $xml_file = <$files>;
-        last unless defined $xml_file;
-        $self->enqueue( $xml_file );
+    for( 1, 2 ) {
+	if( my $xml_file = $self->find_handle->getline ) {
+	    chomp $xml_file;
+	    $self->enqueue( $xml_file );
+	}
     }
 }
 
@@ -176,9 +160,9 @@ sub worker_done {
 # sub worker_manager_stop  { warn 'stopped worker manager' }
 # sub max_workers_reached  { warn 'max workers reached' }
 
-# sub worker_stdout  { shift; warn join ' ', @_; }
-# sub worker_stderr  { shift; warn join ' ', @_; }
-# sub worker_error   { shift; warn join ' ', @_; }
+sub worker_stdout  { shift; warn join ' ', @_; }
+sub worker_stderr  { shift; warn join ' ', @_; }
+sub worker_error   { shift; warn join ' ', @_; }
 # sub worker_started { shift; warn join ' ', @_; }
 # sub sig_child      { shift; warn join ' ', @_; }
 
