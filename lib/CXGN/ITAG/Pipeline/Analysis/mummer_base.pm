b731f8707308b8b3260a19633b2f7983830e2e28
Robert Buels <rmb32@cornell.edu>
2010-10-15 14:14:17 -0700

    added ITAG analysis base class for using mummer to align other genomes to the reference

---
+++ lib/CXGN/ITAG/Pipeline/Analysis/mummer_base.pm
@@ -0,0 +1,239 @@
+package CXGN::ITAG::Pipeline::Analysis::mummer_base;
+use strict;
+use warnings;
+use English;
+use Carp;
+
+use Path::Class;
+use File::Basename;
+
+use Bio::SeqIO;
+
+use CXGN::Tools::Run;
+
+=head1 NAME
+
+CXGN::ITAG::Pipeline::Analysis::mummer_base - pipeline analysis base class for producing alignments using mummer
+
+=head1 BASE CLASS(ES)
+
+L<CXGN::ITAG::Pipeline::Analysis>
+
+=cut
+
+use base qw/CXGN::ITAG::Pipeline::Analysis/;
+
+=head1 METHODS
+
+implements all abstract methods defined in L<CXGN::ITAG::Pipeline::Analysis>,
+
+=cut
+
+
+##### methods to override in subclasses
+sub mummer_params {
+
+    return (
+        [ qw( -mum -b -n -L ) ],
+
+        {
+            -l => 1000,
+        }
+       );
+}
+sub gff3_source {
+    'ITAG_genome'
+}
+sub genome_file {
+    die 'must implement genome_file in subclass!';
+}
+
+
+######## implemented methods
+
+sub locally_runnable { 1 }
+
+sub run {
+  my ($self,$batch) = @_;
+
+  # for each seq file, set up and run an analysis job on the
+  # cluster, format its output, and queue it up to be copied into
+  # place
+  my @jobs;
+  my @ops;
+  for my $seqname ( $batch->seqlist ) {
+      my ( $outfile, $gff3_out_file ) = map $self->cluster_temp($seqname,$_), 'txt', 'gff3';
+
+      my $seq_file = $batch->pipeline->analysis('seq')->files_for_seq( $batch, $seqname );
+
+      my $job = $self->cluster_run_class_method( $batch,
+                                                 $seqname,
+                                                 'run_mummer',
+                                                 $seqname,
+                                                 $self->genome_file,
+                                                 $seq_file,
+                                                 $outfile,
+                                                 $gff3_out_file,
+                                                 #{ err_file => $errfile,
+                                                 #},
+                                                );
+
+      my ($out_dest,$gff_out_dest) = $self->files_for_seq($batch,$seqname);
+      push @jobs, $job;
+      push @ops, [$gff3_out_file => $gff_out_dest ], [ $outfile => $out_dest ];
+
+      # check for any failed jobs, so that we don't have to submit
+      # all the jobs before we die.  submitting all the jobs could
+      # take a very long time if there are many jobs, because the
+      # job submission will block and wait for the torque server to
+      # clear a bit if there are too many jobs in its queue.
+      $_->alive for @jobs;
+  }
+
+  # wait for all the jobs to finish (also running their
+  # on_completion hooks)
+  $_->wait for @jobs;
+
+  #atomically move the results into position
+  $self->atomic_move(@ops);
+}
+
+sub run_mummer {
+    my ( $class, $seqname, $genome_fasta_file, $seq_file, $outfile, $gff3_outfile ) = @_;
+
+    my ($mummer_param_singles, $mummer_param_values) = $class->mummer_params;
+
+    my $mummer_job = CXGN::Tools::Run->run(
+        'mummer',
+
+        @$mummer_param_singles,
+        %$mummer_param_values,
+
+        -F => $genome_fasta_file,
+
+        $seq_file,
+
+        { die_on_destroy => 1,
+          working_dir => File::Spec->tmpdir,
+          on_completion => sub {
+              my $job = shift;
+              my $mummer_results = $class->_parse_mummer( $job->out_file );
+
+              # print our outfile
+              my $job_out = file( $job->out_file )->openr;
+              my $out_fh  = file( $outfile )->openw;
+              $out_fh->print( $_ ) while <$job_out>;
+
+              #convert the mummer results to gff3
+              my $gff_fh = file($gff3_outfile)->openw;
+              $class->_mummer_to_gff3( $mummer_results, $gff_fh );
+          },
+        },
+       )
+      or die "failed to run mummer on fasta files '$genome_fasta_file' versus '$seq_file'";
+
+}
+
+sub _mummer_to_gff3 {
+    my ( $class, $mummer_results, $gff_fh ) = @_;
+
+    $gff_fh->print(<<'');
+##gff3-version 3
+
+    return unless @$mummer_results;
+
+    my $seq_length = $mummer_results->[0]->{q_seq_len}
+        or die "must run mummer with -L switch to include sequence lengths in output";
+
+    $gff_fh->print(<<"");
+##sequence-region $mummer_results->[0]->{query} 1 $seq_length
+
+    for my $match ( @$mummer_results ) {
+        my ( $start, $end ) = ( $match->{q_start}, $match->{q_start} + $match->{match_len} - 1 );
+        ### start: $start
+        ### end: $end
+        ### match: $match
+
+        if ( $match->{query_reversed} ) {
+            ( $start, $end ) = ( $end, $start );
+            $_ = $match->{q_seq_len} - $_ + 1 for $start, $end;
+        }
+
+        $gff_fh->print( join( "\t",
+                              $match->{query},
+                              $class->gff3_source,
+                              'match',
+                              $start,
+                              $end,
+                              '.',
+                              ( $match->{query_reversed} ? '-' : '+' ),
+                              '.',
+                              join( ';',
+                                    'Name='.$match->{subject},
+                                    'Target='
+                                      .$match->{subject}
+                                      .' '.$match->{s_start}
+                                      .' '.($match->{s_start} + $match->{match_len} - 1 )
+                                      .' +'
+                                  ),
+                             ),
+                        "\n"
+                       );
+    }
+
+    return;
+}
+
+
+# returns arrayref as
+#  [
+#    { match info }
+#    ...
+#  ]
+sub _parse_mummer {
+    my ( $class, $mummer_output_file ) = @_;
+
+    open (my $f, $mummer_output_file)
+        or die "Can't open file $mummer_output_file for reading...";
+
+    my @return;
+
+    my $query = "";
+    my $reverse;
+    my $qlength;
+    local $_;
+    while ( my $line = <$f>) {
+        chomp $line;
+
+        if ( $line =~ / ^> \s* (\S+) \s+ (Reverse)? \s* Len \s* = \s* (\d+) /xi ) {
+            ( $query, $reverse, $qlength ) = ( $1, $2, $3 );
+            $reverse = $reverse ? 1 : 0;
+        } else {
+            $line =~ s/^\s+|\s+$//g;
+            my ($subject, $query_start, $subject_start, $length) = split /\s+/, $line;
+
+            push @return, {
+                query          => $query,
+                query_reversed => $reverse,
+                subject        => $subject,
+                q_start        => $query_start,
+                s_start        => $subject_start,
+                match_len      => $length,
+                q_seq_len      => $qlength,
+            };
+        }
+    }
+
+    return \@return;
+}
+
+
+=head1 AUTHOR(S)
+
+Robert Buels
+
+=cut
+
+###
+1;#do not remove
+###
---
+++ t/mummer_base.t
@@ -0,0 +1,186 @@
+use strict;
+use warnings;
+use Test::More;
+
+use IO::Scalar;
+
+use File::Temp;
+use Path::Class;
+
+use String::Random;
+
+use aliased 'CXGN::ITAG::Pipeline::Analysis::mummer_base';
+
+{
+    my $mummer_text = <<'';
+> SL2.30ch00  Len = 21839854
+  C03HBa0060B10.1     71156  16474710      2763
+> SL2.30ch00 Reverse  Len = 21839854
+  C03SLe0022L21.1     17275   4332044      1171
+  C03SLm0006J07.1     89937   6074107      1108
+  C03SLm0006J07.1     38337  11594424      3240
+  C03SLm0006J07.1     43100  11598967      1865
+  C03SLm0006J07.1     45616  11601560      3324
+  C03SLm0006J07.1     51129  11606240      5319
+  C03SLm0006J07.1     57850  11612414      1139
+  C03SLm0006J07.1     59867  11614703      6960
+
+    my $mummer_results =
+        [
+            {
+                'match_len'      => '2763',
+                'q_seq_len'      => '21839854',
+                'q_start'        => '71156',
+                'query'          => 'SL2.30ch00',
+                'query_reversed' => 0,
+                's_start'        => '16474710',
+                'subject'        => 'C03HBa0060B10.1'
+            },
+            {
+                'match_len'      => '1171',
+                'q_seq_len'      => '21839854',
+                'q_start'        => '17275',
+                'query'          => 'SL2.30ch00',
+                'query_reversed' => 1,
+                's_start'        => '4332044',
+                'subject'        => 'C03SLe0022L21.1'
+            },
+            {
+                'match_len'      => '1108',
+                'q_seq_len'      => '21839854',
+                'q_start'        => '89937',
+                'query'          => 'SL2.30ch00',
+                'query_reversed' => 1,
+                's_start'        => '6074107',
+                'subject'        => 'C03SLm0006J07.1'
+            },
+            {
+                'match_len'      => '3240',
+                'q_seq_len'      => '21839854',
+                'q_start'        => '38337',
+                'query'          => 'SL2.30ch00',
+                'query_reversed' => 1,
+                's_start'        => '11594424',
+                'subject'        => 'C03SLm0006J07.1'
+            },
+            {
+                'match_len'      => '1865',
+                'q_seq_len'      => '21839854',
+                'q_start'        => '43100',
+                'query'          => 'SL2.30ch00',
+                'query_reversed' => 1,
+                's_start'        => '11598967',
+                'subject'        => 'C03SLm0006J07.1'
+            },
+            {
+                'match_len'      => '3324',
+                'q_seq_len'      => '21839854',
+                'q_start'        => '45616',
+                'query'          => 'SL2.30ch00',
+                'query_reversed' => 1,
+                's_start'        => '11601560',
+                'subject'        => 'C03SLm0006J07.1'
+            },
+            {
+                'match_len'      => '5319',
+                'q_seq_len'      => '21839854',
+                'q_start'        => '51129',
+                'query'          => 'SL2.30ch00',
+                'query_reversed' => 1,
+                's_start'        => '11606240',
+                'subject'        => 'C03SLm0006J07.1'
+            },
+            {
+                'match_len'      => '1139',
+                'q_seq_len'      => '21839854',
+                'q_start'        => '57850',
+                'query'          => 'SL2.30ch00',
+                'query_reversed' => 1,
+                's_start'        => '11612414',
+                'subject'        => 'C03SLm0006J07.1'
+            },
+            {
+                'match_len'      => '6960',
+                'q_seq_len'      => '21839854',
+                'q_start'        => '59867',
+                'query'          => 'SL2.30ch00',
+                'query_reversed' => 1,
+                's_start'        => '11614703',
+                'subject'        => 'C03SLm0006J07.1'
+            }
+        ];
+
+    {
+        my $f = File::Temp->new;
+        $f->print( $mummer_text );
+        $f->close;
+        my $r = mummer_base->_parse_mummer( $f );
+        is_deeply( $r,
+                   $mummer_results,
+                   'mummer parse works',
+                  )
+            or diag explain $r;
+    }
+
+
+    my $test_gff3 = '';
+    mummer_base->_mummer_to_gff3(
+        $mummer_results,
+        IO::Scalar->new( \$test_gff3 ),
+       );
+
+    is( $test_gff3, <<"", 'got right test gff3' );
+##gff3-version 3
+##sequence-region SL2.30ch00 1 21839854
+SL2.30ch00	ITAG_genome	match	71156	73918	.	+	.	Name=C03HBa0060B10.1;Target=C03HBa0060B10.1 16474710 16477472 +
+SL2.30ch00	ITAG_genome	match	21821410	21822580	.	-	.	Name=C03SLe0022L21.1;Target=C03SLe0022L21.1 4332044 4333214 +
+SL2.30ch00	ITAG_genome	match	21748811	21749918	.	-	.	Name=C03SLm0006J07.1;Target=C03SLm0006J07.1 6074107 6075214 +
+SL2.30ch00	ITAG_genome	match	21798279	21801518	.	-	.	Name=C03SLm0006J07.1;Target=C03SLm0006J07.1 11594424 11597663 +
+SL2.30ch00	ITAG_genome	match	21794891	21796755	.	-	.	Name=C03SLm0006J07.1;Target=C03SLm0006J07.1 11598967 11600831 +
+SL2.30ch00	ITAG_genome	match	21790916	21794239	.	-	.	Name=C03SLm0006J07.1;Target=C03SLm0006J07.1 11601560 11604883 +
+SL2.30ch00	ITAG_genome	match	21783408	21788726	.	-	.	Name=C03SLm0006J07.1;Target=C03SLm0006J07.1 11606240 11611558 +
+SL2.30ch00	ITAG_genome	match	21780867	21782005	.	-	.	Name=C03SLm0006J07.1;Target=C03SLm0006J07.1 11612414 11613552 +
+SL2.30ch00	ITAG_genome	match	21773029	21779988	.	-	.	Name=C03SLm0006J07.1;Target=C03SLm0006J07.1 11614703 11621662 +
+
+
+    my $temp_out  = File::Temp->new;
+    my $temp_gff3 = File::Temp->new;
+
+    my $random_dna = random_dna(5000);
+    my $genome_fasta = tempfile_containing( ">FooGenome\n$random_dna\n" );
+
+    my $seq_file = tempfile_containing( ">BarSequence\n".substr( $random_dna, 123, 4222 )."\n" );
+
+    mummer_base->run_mummer( 'BarSequence', $genome_fasta, $seq_file, $temp_out, $temp_gff3 );
+    is( file($temp_out)->slurp, <<'', 'right mummer out' );
+> BarSequence  Len = 4222
+  FooGenome       124         1      4222
+> BarSequence Reverse  Len = 4222
+
+
+    is( file($temp_gff3)->slurp, <<'', 'right mummer gff3' );
+##gff3-version 3
+##sequence-region BarSequence 1 4222
+BarSequence	ITAG_genome	match	124	4345	.	+	.	Name=FooGenome;Target=FooGenome 1 4222 +
+
+
+}
+
+done_testing;
+
+
+sub tempfile_containing {
+    my ($contents) = shift;
+
+    my $f = File::Temp->new;
+    $f->print($contents);
+    $f->close;
+    return $f;
+}
+
+sub random_dna {
+    my ($length) = @_;
+    my $r = String::Random->new;
+    $r->{D} = [qw[ A C T G ]];
+    return $r->randpattern('D'x$length);
+}
