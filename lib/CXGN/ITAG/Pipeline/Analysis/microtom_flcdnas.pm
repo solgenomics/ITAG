7ba02cdec3e6dc34b786a6851bb90e7623d5bfab
Robert Buels <rmb32@cornell.edu>
2010-12-09 14:52:57 -0500

    bugfixes to microtom itag analysis

--- lib/CXGN/ITAG/Pipeline/Analysis/microtom_flcdnas.pm
+++ lib/CXGN/ITAG/Pipeline/Analysis/microtom_flcdnas.pm
@@ -4,6 +4,8 @@ use warnings;
 
 use CXGN::Tools::Wget qw/ wget_filter /;
 
+use base 'CXGN::ITAG::Pipeline::Analysis::gth_base';
+
 sub locally_runnable { 1 }
 
 sub _source {
@@ -14,10 +16,14 @@ sub _cdna_file {
     my $class = shift;
     wget_filter(
         'ftp://ftp.solgenomics.net/genomes/Solanum_lycopersicum/microtom_flcdna/HTC13227.seq.gz'
-            => $class->local_temp('microtom_flcdnas.fasta')
-           );
+            => $class->local_temp('microtom_flcdnas.fasta'),
+	{ gunzip => 1 },
+	);
 }
 
 sub _parse_mode {
     'alignments_merged'
 }
+
+1;
+
