0a0ea090b07f61a6b23152da9b4207d08859860e
Robert Buels <rmb32@cornell.edu>
2010-12-10 17:42:36 -0500

    unecessary use in itag1_ref

--- lib/CXGN/ITAG/Pipeline/Analysis/itag1_ref.pm
+++ lib/CXGN/ITAG/Pipeline/Analysis/itag1_ref.pm
@@ -2,7 +2,6 @@ package CXGN::ITAG::Pipeline::Analysis::itag1_ref;
 use strict;
 use warnings;
 use base qw/CXGN::ITAG::Pipeline::Analysis::mummer_base/;
-use CXGN::Tools::Wget qw/ wget_filter /;
 
 sub gff3_source {
     'ITAG_SL1.00_scaffolds'
